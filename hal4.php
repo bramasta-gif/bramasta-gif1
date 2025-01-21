f70d971c6e2d4899101c7ec38d72063d30b8c4bc
bramasta-gif <bramastalubis@gmail.com>
2025-01-21 22:00:36 +0700

    Add files via upload

---
+++ hal4.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+  
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+
+    <style>
+      body {
+        background-image: url("gambar/20.jpg");
+      }
+    </style>
+
+    <title>Sayang</title>
+  </head>
+
+  <body>
+    <audio controls="autoplay-loop" src="gambar/ok.mp3" type="audio/mpeg" style="display:none;" class="active" >
+    </audio>
+    <!-- Kode Carousel -->
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+          </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="gambar/51.png" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="gambar/52.png" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="gambar/53.png" alt="Third slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="gambar/54.png" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div> 
+
+      <h3 style="text-align: center; margin-top: 1rem; font-family: script bold italic; ">HAPPY ANNIVERSARY</h3>
+<br>
+
+  <div class="container" style= "width: 90%; margin: 0 auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+     <div class="photo-container" style=" display: flex; flex-direction: column; align-items: center; grid-gap: 20px;">
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/1.png" alt="Photo 1"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/2.png" alt="Photo 2"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/3.png" alt="Photo 3"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/4.png" alt="Photo 4"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/5.png" alt="Photo 5"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/6.png" alt="Photo 6"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/7.png" alt="Photo 7"></div>
+      <div class="photo-container img" style="object-fit: cover; border: 2px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); transition: transform 0.3s;"> 
+       <div class="photo-container img:hover" style=" transform: scale(1.05);"></div>
+       <img class="d-block w-100" src="gambar/8.png" alt="Photo 8"></div>
+      </div>
+     </div>
+  </div>
+
+<br>
+
+  <div class="container">
+      <div class="card border" style=" margin: auto ">
+        <h5 class="card-header" style="text-align: center; background-color: #ff69b4"><b>Terima Kasih Sayang<b></h5>
+        <div class="card-body" style="background-color: #ffb6c1">
+          <p class="card-text" style="text-align: center;">"Tidak terasa waktu telah membawa kita sejauh ini, melewati hari-hari yang penuh dengan kebahagiaan, tantangan, tawa, dan air mata, yang semuanya justru membuat cinta kita semakin kokoh dan indah; di setiap langkah yang kita tempuh bersama, aku semakin menyadari bahwa kamulah anugerah terbesar yang pernah hadir dalam hidupku, seseorang yang melengkapi setiap kekuranganku, menerima setiap kelebihanku, dan menjadikan setiap momen sederhana terasa luar biasa; hari ini, di momen yang istimewa ini, aku ingin mengucapkan terima kasih atas cinta, kesabaran, dan kebahagiaan yang kau berikan, serta berjanji untuk terus mencintaimu dengan sepenuh hati, lebih dari hari kemarin, dan bahkan lebih dari yang bisa kuungkapkan dengan kata-kata selamat anniversary, sayang, aku mencintaimu lebih dari yang pernah bisa kau bayangkan."
+          </p>
+  </div>    
+</div>
+</div>
+
+<br>
+
+        <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="width: 100%; background-color: rgba(0, 0, 0, 0.2)">
+        You are logged<br>
+        © 2025 Copyright
+        </div>
+        <!-- Copyright -->
+        </footer>     
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/style.css">
+
+    <style>
+      body {
+        background-image: url("gambar/20.jpg");
+      }
+    </style>
+
+    <title>Sayang</title>
+  </head>
+  <body>
+    <div class="countainer d-flex justify-content-center vh-100 align-items-center">
+      <div class="card" style="width: 350px; background: #aebaf8; margin: 20px auto; padding: 30px 20px;">
+        <div class="text-center">
+          <img src="gambar/ya.png">
+          <h1 id="yryr" class="mb-0">Bilang Sayang Untuk Lanjut</h1>
+          <div class="mb-4 utju "></div>
+          
+             <form style="box-sizing: border-box; width: 100%; padding: 10px; font-size: 11pt; margin-bottom: 20px;" 
+                    
+              button class="btn btn-primary" type="submit" style="background: #46de4b; color: white; font-size: 18pt; width: 100%; border: none; border-radius: 3px; padding: 10px 20px;"><a button class="btn btn-primary" href="hal1.php"><b>SAYANG &#x2764<b></a>
+
+             </form>
+         
+        </div>
+      </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+    </div>
+  </body>
+</html>
